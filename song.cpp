76db0bc588c4ee484f695a6d6414412b8e72a5e8
tylerstern0 <32274357+tylerstern0@users.noreply.github.com>
2018-10-30 01:32:06 -0500

    song files initial upload

--- song.cpp
+++ song.cpp
@@ -1,5 +1,59 @@
 //
-// Created by Aditya Singh on 10/30/18.
+// Created by Tyler Stern on 10/28/18.
 //
 
 #include "song.h"
+
+
+
+//4 different constructors; we'll likely only ever use the last one
+Song::Song()
+{
+    name = "";
+    artist = "";
+    size = DEFAULT_SIZE;
+}
+
+Song::Song(string name)
+{
+    this->name = name;
+    artist = "";
+    size = DEFAULT_SIZE;
+}
+
+Song::Song(string _name, string _artist){
+    name = _name;
+    artist = _artist;
+    size = DEFAULT_SIZE;
+}
+
+Song::Song(string _name, string _artist, int _size)
+{
+    name = _name;
+    artist = _artist;
+    size = _size;
+}
+
+
+//get and set instance variables (name, artist, size)
+string Song::getName() const {
+    return name;
+}
+void Song::setName(string n) {
+    name = n;
+}
+string Song::getArtist() const {
+    return artist;
+}
+
+void Song::setArtist(string a) {
+    artist = a;
+}
+int Song::getSize() const {
+    return size;
+}
+
+void Song::setSize(int n) {
+    name = n;
+}
+
--- song.h
+++ song.h
@@ -1,8 +1,44 @@
 //
-// Created by Aditya Singh on 10/30/18.
+// Created by Tyler Stern on 10/28/18.
 //
 
-#ifndef UTPOD_SONG_H
-#define UTPOD_SONG_H
+#ifndef SONG_H
+#define SONG_H
 
-#endif //UTPOD_SONG_H
+#include <string>
+
+
+using namespace std;
+
+class Song {
+
+private:
+    string name;
+    string artist;
+    int size;
+
+    static const int DEFAULT_SIZE = 0;
+
+public:
+    //constructors
+    Song();
+    Song(string name);
+    Song(string _name, string _artist);
+    Song(string _name, string _artist, int _size);
+
+
+    //get and set instance variables (name, artist, size)
+    string getName() const;
+    void setName(string n);
+    string getArtist() const;
+    void setArtist(string a);
+    int getSize() const;
+    void setSize(int n);
+
+    //destructor
+    ~Song(Song &s);
+};
+
+
+
+#endif SONG_H
