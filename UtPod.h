f8ab341caca848478913939e11b1e54412dbafef
adityasingh1 <adityasingh@utexas.edu>
2018-10-31 19:16:40 -0500

    Header
    Signed-off-by: adityasingh1 <adityasingh@utexas.edu>

--- UtPod.h
+++ UtPod.h
@@ -5,31 +5,61 @@
 #ifndef UTPOD_UTPOD_H
 #define UTPOD_UTPOD_H
 
-#endif //UTPOD_UTPOD_H
 #include <string>
 
 using namespace std;
 
-class UtPod{
+class UtPod {
 
 private:
 
+    int memory;
+    *Node head;
+    static const int DEFAULT_MEMORY = 0;
 
+    void swap(*Node Node1, *Node Node2);
 
+public:
 
+    // constructors
 
+    UtPod();
 
+    UtPod(int _memory);
 
+    // methods
 
+    bool addSong(Song s);
 
+    bool removeSong(Song s);
 
+    void shuffle();
 
+    void showSongList() const;
 
+    void sortSongList();
 
+    int getTotalMemory() const;
 
+    int getRemainingMemory() const;
 
+    // destructors
 
-
+    ~UtPod();
 
 };
+
+#endif //UTPOD_UTPOD_H
+
+
+
+
+
+
+
+
+
+
+
+
 
